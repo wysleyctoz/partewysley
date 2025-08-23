619d6b909544cd89f64a9c3b0b7792e1bea2eb80
wysleyctoz <wlcs2@aluno.ifal.edu.br>
2025-08-23 14:33:45 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'hoteis.dart';
+import 'favoritos.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HotelScreen(),
+  ));
+}
